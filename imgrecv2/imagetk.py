05d646f51679da87a6bf1fc451cb44cb215a788b
Kai Xu <71488741+KXpress@users.noreply.github.com>
2022-02-06 09:03:46 +0800

    comments!

--- imgrecv2/imagetk.py
+++ imgrecv2/imagetk.py
@@ -3,18 +3,18 @@ from tkinter import *
 from tkinter import filedialog
 from tkinter import ttk
 from tkinter.filedialog import askopenfile
-from PIL import Image, ImageTk
-import tkinter
-from scipy.spatial import distance as dist
+import tkinter #guis (tkinter)
+from PIL import Image, ImageTk #to display image
 from imutils import perspective
-from imutils import contours
-import numpy as np
-import cv2
+from imutils import contours #to check for contours/ object detect
+import numpy as np #for arrays
+import cv2 
 import imutils
-import cv2 as cv
-from skimage.metrics import structural_similarity as ssim
-import random
+import cv2 as cv #opencv
+from skimage.metrics import structural_similarity as ssim #ssim comparison 
+import random #radomisation
 
+#Main page/ front page
 newfont = ("Open Sans", 20,"bold")
 newfont1 = ("Open Sans",15)
 canvas = tk.Tk()
@@ -30,7 +30,7 @@ l3=tk.Label(canvas,text="Cost of product (Optional): ")
 l3.place(x=20,y=210)
 l3.configure(font=newfont1)
 b1 = tk.Button(canvas, text='Upload Image', 
-   width=20,command = lambda:upload_file())
+   width=20,command = lambda:upload_file()) #button to upload file
 b1.place(x=220,y=60)
 yourimg = Image.open("yourimg.jpeg") #load image
 yourimg = yourimg.resize((100, 100))
@@ -45,7 +45,7 @@ l6.place(x=220,y=260)
 l8 = tk.Label(canvas,text="Upload an image with a coin and identify it with a click!",font=("Open Sans",12))
 l8.place(x=170,y=30)
 
-imagefile=StringVar()
+imagefile=StringVar() #set string variable for the imagefile
 coind,perc=[],[]
 
 #GO TO OTHER PAGES DIRECTORIES
@@ -64,7 +64,7 @@ def upload_file():
     global imagefile #takes in variable from outside function
     f_types = [('Jpg Files', '*.jpg'),
     ('PNG Files','*.png'),
-    ('Jpeg Files','*.jpeg')]   # type of files to select 
+    ('Jpeg Files','*.jpeg')]   # type of files accepted only
     filename = tk.filedialog.askopenfilename(multiple=True,filetypes=f_types)
     imagefile = filename #flename
     for f in filename:
@@ -80,38 +80,41 @@ def upload_file():
 options = ["Dollars ($)", "Cents (¢)"]        
 clicked=StringVar()
 clicked.set(options[0])
-drop = OptionMenu(canvas, clicked, *options)#,command=selected)
+drop = OptionMenu(canvas, clicked, *options) #dropdown menu
 drop.pack(pady=20)
 drop.place(x=220,y=215)
 costin=StringVar()
 costinp=tk.Entry(canvas,textvariable=costin)
 costinp.place(x=220,y=235)
+
+#=== Validation check for input of image/ input for the cost ===
 def submit1():
         global imagefile
         a=False
-        inputs=str(costin.get()).split(".")
-        if len(inputs)<3:
+        inputs=str(costin.get()).split(".") 
+        if len(inputs)<3: #if dollar sign, only can have one decimal
             for i in inputs:
-                if str(i).isdigit()==True:
+                if str(i).isdigit()==True: #check for alpha, return false
                     a=True
                 else:
                     a=False
                     break
-        if costin.get()=="":
+        if costin.get()=="": #no cost input
             a=True
-        if len(inputs)>1 and clicked.get()=="Cents (¢)":
+        if len(inputs)>1 and clicked.get()=="Cents (¢)": #cents cannot have decimal
             a=False
         if str(imagefile)!="":
             if str(imagefile)[0]!="(": #check if there is any image input
                 a=False
         return a
 
-def nothing(): #NO COIN DETECTED #maybe need to overlay instead
+# === WINDOW WHEN NO OBJECT/ COIN DETECTED ===
+def nothing(): 
     canvas.destroy()
     resultf=tk.Tk()
     resultf.title("Coint: Computer Identification Result")
     resultf.geometry("600x400") #window dimension
-    resultf.geometry("+550+250") #making the window centre #!!!
+    resultf.geometry("+550+250") #making the window centre
     lhead = tk.Label(resultf,text='Computer Identification',font=("Open Sans",20,"bold"))
     lhead.place(x=180,y=0)
     errimg = Image.open("coinerror.png")
@@ -136,7 +139,8 @@ def nothing(): #NO COIN DETECTED #maybe need to overlay instead
     gbackb.place(x=420,y=350)
     resultf.mainloop()
 
-def something(): #COIN DETECTED
+# === WINDOW WHEN A COIN/ OBJECT IS FOUND ===
+def something():
     canvas.destroy()
     global coind
     global perc
@@ -144,7 +148,7 @@ def something(): #COIN DETECTED
     resultp=tk.Tk()
     resultp.title("Coint: Computer Identification Result")
     resultp.geometry("600x400") #window dimension
-    resultp.geometry("+550+250") #making the window centre #!!!
+    resultp.geometry("+550+250") #making the window centre
     lhead = tk.Label(resultp,text='Computer Identification',font=("Open Sans",20,"bold"))
     lhead.place(x=180,y=0)
     bgimg = Image.open("backrec.png")
@@ -166,7 +170,6 @@ def something(): #COIN DETECTED
                             break
                     else:
                             pos+=1
-    #print(totalperc)
     tperccopy=totalperc.copy()
     tperccopy.sort()
     tperccopy=tperccopy[::-1]
@@ -175,13 +178,11 @@ def something(): #COIN DETECTED
         totalpos.append(coinlist[x])
         totalperc[x]="i"
         totalpos.append(i)
-    #print(totalpos)
-    ans=str(totalpos[0]).split("_")
+    ans=str(totalpos[0]).split("_") #convert image filename to strname for display
     ansstr=""
-    perans=round(totalpos[1]*100,2)
-    #print(perans)
+    perans=round(totalpos[1]*100,2) #percentage for display
     perans=str(perans)
-    for i in range (len(ans)):
+    for i in range (len(ans)): #convert image filename to strname for display
         ans[i]=str(ans[i]).capitalize()
     if len(ans)==3:
         ans[2]="(Old)"
@@ -196,12 +197,12 @@ def something(): #COIN DETECTED
 
     inpimg = Image.open(imagefile[0])
     inpimg = inpimg.resize((120, 120))
-    inpimg = ImageTk.PhotoImage(inpimg)
+    inpimg = ImageTk.PhotoImage(inpimg) #display input image
     inppanel = tkinter.Label(resultp, image = inpimg)
     inppanel.image=inpimg
     inppanel.place(x=40,y=115)
     
-    ansimgn=str(totalpos[0])+"0.png"
+    ansimgn=str(totalpos[0])+"0.png" #display coin indetified image
     ansimg = Image.open(ansimgn)
     ansimg = ansimg.resize((120, 120))
     ansimg = ImageTk.PhotoImage(ansimg)
@@ -214,18 +215,18 @@ def something(): #COIN DETECTED
     resl.place(x=300,y=115)
     lines=[3,7,11,15,19]
     acoin=['five_cent','ten_cent','twenty_cent','fifty_cent','one_dollar','five_cent_old','ten_cent_old','twenty_cent_old','fifty_cent_old','one_dollar_old']
-    des=open("coindesc.txt")
+    des=open("coindesc.txt") #txt file for all the coins
     descon=des.readlines()
     num=acoin.index(totalpos[0])
     if num>4:
         num-=5
-    desc=descon[num]
+    desc=descon[num] #line for desc of the coin found
     des.close()
     descl=tk.Label(resultp,text=desc,wraplength=270,justify="left",font=("Open Sans",14))
     descl.place(x=300,y=140)
     coinv=[0.05,0.10,0.20,0.50,1.00]
     formcost=coinv[num]
-    inputcost=costin.get()
+    inputcost=costin.get() #get the input cost format d/c and the value
     formatcost=clicked.get()
     textcost=""
     changecost,colour1,changing,dol,cen=0,"blue","Change: ","$","¢"
@@ -233,15 +234,15 @@ def something(): #COIN DETECTED
     if inputcost!="":
         if formatcost=="Cents (¢)":
             inputcost=float(inputcost)
-            inputcost/=100
-            dol=""
+            inputcost/=100 #change to dollars first for calculations
+            dol="" #set to nothing as cents is set
         if formatcost=="Dollars ($)":
-            cen=""
+            cen="" #set to nothing as dollar is set
             if str(inputcost).find(".")>0:
                 inputcost=str(inputcost)
                 if len(inputcost[inputcost.find(".")+1:])<2:
-                    inputcost+="0"
-        changecost=float(formcost)-float(inputcost)
+                    inputcost+="0" #leading zero
+        changecost=float(formcost)-float(inputcost) #calculate dif
         if changecost==0:
             textcost="You have just enough money."
         elif changecost>0:
@@ -250,17 +251,16 @@ def something(): #COIN DETECTED
             textcost="You do not have enough money."
             colour1="#ff7700"
             changing="Short of: "
-        changecost=abs(round(changecost,2))
+        changecost=abs(round(changecost,2)) #change to positive number
         if formatcost=="Cents (¢)":
             inputcost=inputcosttemp
-            changecost*=100
+            changecost*=100 #conver back to cents
             changecost=int(changecost)
         elif str(changecost).find(".")>0:
             changecost=str(changecost)
             if len(changecost[changecost.find(".")+1:])<2:
-                changecost+="0"
-        
-        #print("{}\n{}\n{}\n{}\n{}".format(formcost,inputcost,formatcost,textcost,changecost))
+                changecost+="0" #leading zero
+
         cosl = tk.Label(resultp,text="Balance",font=("Open Sans",15,"bold"))
         cosl.place(x=300,y=260)
         inpcostl=tk.Label(resultp,text=("Cost Input: "+dol+str(inputcost)+cen),font=("Open Sans",14))
@@ -272,7 +272,7 @@ def something(): #COIN DETECTED
     otherposl=tk.Label(resultp,text="Other Possibilities",font=("Open Sans",15,"bold"))
     otherposl.place(x=30,y=255)
     namecoin5,valcoin5=[],[]
-    for i in range(10):
+    for i in range(10): #to arrange the top 5 coins and values into the lists namecoin5 and valuecoin5
         if i%2==0:
             ans5=str(totalpos[i]).split("_")
             ansstr5=""
@@ -307,16 +307,16 @@ def something(): #COIN DETECTED
     gbackb.place(x=420,y=368)
     
 
-# ALGO TO IDENTIFY IMG
+# === ALGO TO IDENTIFY IMG ====
 def algo(imginput):
     global coind
     global perc
     while True:
-            # load image, black and white
+            # change image to black and white
             image = cv2.imread(imginput)
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             gray = cv2.GaussianBlur(gray, (7, 7), 0)
-            # detect the edges of object
+            # find the edges in the image
             edged = cv2.Canny(gray, 50, 100)
             edged = cv2.dilate(edged, None, iterations=1)
             edged = cv2.erode(edged, None, iterations=1)
@@ -328,29 +328,32 @@ def algo(imginput):
             if len(cnts)==0: #BREAK, NO OBJECT detected (e.x. plain black colour pic)
                     nothing()
                     break
-            # sort the contours
+            # sort contours based on the array and their coord
             (cnts, _) = contours.sort_contours(cnts)
             colors = ((0, 0, 255), (240, 0, 159), (255, 0, 0), (255, 255, 0))
             coord,area,coordmax=[],[],[]
-            for (i, c) in enumerate(cnts):
+            for (i, c) in enumerate(cnts): #goes through all the contours 1 by 1
                     if cv2.contourArea(c) < 1000: #if area too small, ignore
                             continue
                     # draw contours
                     box = cv2.minAreaRect(c)
-                    box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+                    if imutils.is_cv2():
+                        box = cv2.cv.BoxPoints(box)
+                    else:
+                        box = cv2.boxPoints(box)
                     box = np.array(box, dtype="int")
                     cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
-                    area.append(cv2.contourArea(c))
+                    area.append(cv2.contourArea(c)) #append the coordinates and area of the boundaries for comparison and comparing later
                     coord.append(box.astype("int"))
             if len(area)>5: #want top 5 area covered
                     areatemp=area.copy()
                     areatemp.sort() #sorts area covered
                     areatemp=areatemp[::-1]
                     for i in range (5):
-                            coordmax.append(coord[area.index(areatemp[i])])
+                            coordmax.append(coord[area.index(areatemp[i])]) #finds the top 5 largest areas coordinates
             else:
                     coordmax=coord
-            if len(coord)==0: #BREAK, no significant object detected, break
+            if len(coord)==0: #BREAK, no significant object detected, where no objects are even found, break
                     nothing()
                     break
             x_coord,y_coord=[],[]
@@ -360,42 +363,40 @@ def algo(imginput):
                             c=c[1:(len(c)-1)]
                             x,y=c.split()
                             x_coord.append(int(x))
-                            y_coord.append(int(y))
+                            y_coord.append(int(y)) #sorts the x and y coordinates of each boundary
 
             # === CROPPING IMAGE TO FIT OBJECT/ COIN ===
             orgimg = cv2.imread(imginput)
             points=[min(y_coord),max(y_coord), min(x_coord),max(x_coord)]
             for i in range (4):
                     if i==0 or i==2:
-                            if points[i]<0:
+                            if points[i]<0: #if coordinate is outside the image, set to zero
                                     points[i]=0
-                    elif i==1:
+                    elif i==1: #if coordinate is outside image, set to the max height of image
                             if points[1]>orgimg.shape[1]:
                                     points[1]=orgimg.shape[1]
-                    elif i==3:
+                    elif i==3: #if coorfinate is outside image, set to max width of image
                             if points[3]>orgimg.shape[0]:
                                     points[3]=orgimg.shape[0]
-            cropped_image = orgimg[points[0]:points[1], points[2]:points[3]]
+            cropped_image = orgimg[points[0]:points[1], points[2]:points[3]] #crop image
             cv2.imwrite("cropimg.png", cropped_image)
 
             # === LOOK FOR CIRCLE --> COIN ===
             cimg=cv2.imread("cropimg.png")
-            gray = cv2.cvtColor(cimg, cv.COLOR_BGR2GRAY)
+            gray = cv2.cvtColor(cimg, cv.COLOR_BGR2GRAY) #change to black and white
             gray = cv.medianBlur(gray, 5)
             rows = gray.shape[0]
-            rad=int(0.4*min(cimg.shape[:2]))
+            rad=int(0.4*min(cimg.shape[:2])) #minimum radius is 40% of the smallest side of the image
             circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 8,
                                        param1=100, param2=30,
-                                       minRadius=rad, maxRadius=rad+50)
-            if circles is not None:
-                    circles = np.uint16(np.around(circles))
-                    #print("coin found")
+                                       minRadius=rad, maxRadius=rad+50) #look for circles with radius of sort
+            if circles is not None: #if there are circles
                     # === COMPARE IMAGE WITH DATA BASE ===
                     x=[]
                     coinsrank=[]
                     fivo,teno,tweo,fifo,oneo=(['five_cent_old0.png','five_cent_old1.png', 'five_cent_old2.png', 'five_cent_old3.png', 'five_cent_old4.png', 'five_cent_old5.png']),(['ten_cent_old0.png','ten_cent_old1.png', 'ten_cent_old2.png', 'ten_cent_old3.png', 'ten_cent_old4.png', 'ten_cent_old5.png']),(['twenty_cent_old0.png','twenty_cent_old1.png', 'twenty_cent_old2.png', 'twenty_cent_old3.png', 'twenty_cent_old4.png']),(['fifty_cent_old0.png','fifty_cent_old1.png', 'fifty_cent_old2.png', 'fifty_cent_old3.png', 'fifty_cent_old4.png']),(['one_dollar_old0.png','one_dollar_old1.png', 'one_dollar_old2.png', 'one_dollar_old3.png', 'one_dollar_old4.png', 'one_dollar_old5.png'])
                     fiv,ten,twe,fif,one=(['five_cent0.png','five_cent1.png', 'five_cent2.png', 'five_cent3.png', 'five_cent4.png']),(['ten_cent0.png','ten_cent1.png', 'ten_cent2.png', 'ten_cent3.png', 'ten_cent4.png']),(['twenty_cent0.png','twenty_cent1.png', 'twenty_cent2.png', 'twenty_cent3.png', 'twenty_cent4.png', 'twenty_cent5.png']),(['fifty_cent0.png','fifty_cent1.png', 'fifty_cent2.png', 'fifty_cent3.png']),(['one_dollar0.png','one_dollar1.png', 'one_dollar2.png', 'one_dollar3.png', 'one_dollar4.png'])
-                    random.shuffle(fivo) #randomly shuffle
+                    random.shuffle(fivo) #randomly shuffle the lists
                     random.shuffle(teno)
                     random.shuffle(tweo)
                     random.shuffle(fifo)
@@ -405,21 +406,19 @@ def algo(imginput):
                     random.shuffle(twe)
                     random.shuffle(fif)
                     random.shuffle(one)
+                    #randomly get the images to compare with
                     coins = [fivo[0],fivo[3],teno[1],teno[2],tweo[3],tweo[2],fifo[2],fifo[1],oneo[0],oneo[3],fiv[2],fiv[0],ten[1],ten[2],twe[1],twe[0],fif[3],fif[2],one[2],one[1]]
                     for i in range (20):
                         img_org=cv2.imread("cropimg.png")
                         img_org_1=cv2.resize(img_org, (500, 500)) #resize the dimenssions
                         img_test=cv2.imread(coins[i])
                         img_test_1=cv2.resize(img_test, (500, 500)) #resize the dimenssions
-                        s = ssim(img_org_1, img_test_1,  multichannel=True)
+                        s = ssim(img_org_1, img_test_1,  multichannel=True) #ssim test
                         x.append(s)
-                    #print("=== Results ===")
                     y=x.copy()
                     y.sort()
                     for i in y:
-                        coinsrank.append(coins[x.index(i)])
-                    #for i in range (20):
-                        #print("{}. {}: {} %".format(i+1, coinsrank[19-i], format(float(y[19-i]*100),".2f")))
+                        coinsrank.append(coins[x.index(i)]) #link the coin file name with their calculated ssim value that is arranged in descending order
                     coind=coinsrank[::-1]
                     perc=y[::-1]
                     something()
